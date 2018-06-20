b3cb0c8945dcabd13e2c743bea74457a6b80c900
Baptiste Leduc <baptiste@weglot.com>
2018-06-21 00:35:03 +0200

    StyleCI fixes

--- src/Migrations/Version20180619134107.php
+++ src/Migrations/Version20180619134107.php
@@ -15,7 +15,7 @@ final class Version20180619134107 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE TABLE t_user (id UUID NOT NULL, username VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN t_user.id IS \'(DC2Type:uuid)\'');
@@ -24,7 +24,7 @@ final class Version20180619134107 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('DROP TABLE t_user');
     }
--- src/Migrations/Version20180619135304.php
+++ src/Migrations/Version20180619135304.php
@@ -15,7 +15,7 @@ final class Version20180619135304 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE TABLE t_user_character (id UUID NOT NULL, user_id UUID NOT NULL, character_id INT NOT NULL, character_name VARCHAR(255) NOT NULL, main BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_939A3DD0A76ED395 ON t_user_character (user_id)');
@@ -27,7 +27,7 @@ final class Version20180619135304 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE t_user_character DROP CONSTRAINT FK_939A3DD0A76ED395');
