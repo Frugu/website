214b336d6c4c3aa742c4b59bdb60e980b399d2e6
Baptiste Leduc <baptiste@weglot.com>
2018-06-21 18:45:38 +0200

    adding migration for is_admin field

---
+++ src/Migrations/Version20180621164122.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180621164122 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE t_user ADD is_admin BOOLEAN NOT NULL DEFAULT FALSE');
+        $this->addSql('ALTER INDEX idx_939a3dd0a76ed395 RENAME TO IDX_7FDCD773A76ED395');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE t_user DROP is_admin');
+        $this->addSql('ALTER INDEX idx_7fdcd773a76ed395 RENAME TO idx_939a3dd0a76ed395');
+    }
+}
