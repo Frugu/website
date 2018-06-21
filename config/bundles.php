5b593a089308386f4f2400f8cd82b088cb2a46cf
Baptiste Leduc <baptiste@weglot.com>
2018-06-21 22:32:38 +0200

    adding DoctrineFixturesBundle to bundles list

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
