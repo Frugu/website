99a3d3dbe2d3606c0b6b19cb0a7916ec2ea2642b
Baptiste Leduc <baptiste@weglot.com>
2018-06-21 22:33:19 +0200

    adding fixtures for dev purposes

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Auth\UserCharacter;
+use App\Factory\Auth\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $characters = [
+            91901482 => 'Mealtime',
+            93577264 => 'Sandy Stackhouse'
+        ];
+
+        $admins = [
+            91901482
+        ];
+
+        foreach ($characters as $characterId => $characterName) {
+            $userCharacter = new UserCharacter();
+            $userCharacter->setCharacterId($characterId);
+            $userCharacter->setCharacterName($characterName);
+            $userCharacter->setMain(true);
+            $user = UserFactory::createFromCharacter($userCharacter);
+
+            if(in_array($characterId, $admins)) {
+                $user->setIsAdmin(true);
+            }
+
+            $manager->persist($user);
+            $manager->persist($userCharacter);
+        }
+        $manager->flush();
+    }
+}
