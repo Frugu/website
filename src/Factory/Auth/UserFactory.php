beb2d9730f07693669e798c5b56087031be4774b
Baptiste Leduc <baptiste@weglot.com>
2018-06-21 13:25:05 +0200

    creating UserFactory to manage slug-related issues out of entity

---
+++ src/Factory/Auth/UserFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factory\Auth;
+
+use App\Entity\Auth\User;
+use App\Entity\Auth\UserCharacter;
+use Cocur\Slugify\Slugify;
+
+class UserFactory
+{
+    /**
+     * Create an User based on UserCharacter details
+     *
+     * @param UserCharacter $userCharacter
+     * @return User
+     */
+    public static function createFromCharacter(UserCharacter &$userCharacter): User
+    {
+        $characterName = $userCharacter->getCharacterName();
+
+        $user = new User();
+        $user->setUsername($characterName);
+        $user->setSlug(self::getSlugify()->slugify($characterName));
+        $userCharacter->setUser($user);
+
+        return $user;
+    }
+
+    /**
+     * @return Slugify
+     */
+    protected static function getSlugify(): Slugify
+    {
+        return new Slugify();
+    }
+}
