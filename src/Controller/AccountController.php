46fc0fb329fb2afc3d501cbbfbb31d4b48c58d4c
Baptiste Leduc <baptiste@weglot.com>
2018-06-24 15:10:53 +0200

    creating User form for Account page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,6 +14,19 @@ class AccountController extends Controller
      */
     public function index()
     {
-        return $this->render('account/index.html.twig');
+        $user = $this->getUser();
+        $formBuilder = $this->createFormBuilder($user);
+        $formBuilder->add(
+            'username',
+            TextType::class,
+            [
+                'help' => 'My Help Message'
+            ]
+        );
+        $formBuilder->add('update', SubmitType::class);
+
+        return $this->render('account/index.html.twig', [
+            'form' => $formBuilder->getForm()->createView()
+        ]);
     }
 }
