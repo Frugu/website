89c6d0482399f2e94b0f5603275f03f384cf82cd
Baptiste Leduc <baptiste@weglot.com>
2018-07-06 16:45:13 +0200

    cleaning UserType form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,10 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
@@ -17,16 +16,8 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username', TextType::class, [
-                'help' => 'My Help Message',
+                'help' => 'This is the name used in the forum.'
             ])
             ->add('update', SubmitType::class);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        //
     }
 }
