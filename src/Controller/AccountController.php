fdbfc7e0f88f43d66ff741415b8ff9ab930786ee
Baptiste Leduc <baptiste@weglot.com>
2018-07-06 17:16:59 +0200

    updating Account controller to save username changes

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Auth\User;
 use App\Form\UserType;
+use App\Repository\Auth\UserRepository;
+use App\Repository\Exception\UnsupportedClassException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AccountController extends Controller
 {
     /**
      * @Route("/account", name="account")
+     *
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @return Response
+     *
+     * @throws UnsupportedClassException
      */
-    public function index()
+    public function index(Request $request, UserRepository $userRepository)
     {
         $form = $this->createForm(UserType::class, $this->getUser());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            $userRepository->save($user);
+
+            $this->addFlash('success', 'Account updated.');
+        }
 
         return $this->render('account/index.html.twig', [
             'form' => $form->createView(),
