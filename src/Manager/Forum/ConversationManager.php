eeb9d87287a0b4b50dab8619b34279e3899117b3
Baptiste Leduc <baptiste@weglot.com>
2018-07-14 22:53:25 +0200

    adding User & Category objects as required when creating a Conversation instance

--- src/Manager/Forum/ConversationManager.php
+++ src/Manager/Forum/ConversationManager.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Manager\Forum;
 
+use App\Entity\Auth\User;
+use App\Entity\Forum\Category;
 use App\Entity\Forum\Conversation;
 use Cocur\Slugify\Slugify;
 
@@ -14,18 +16,22 @@ class ConversationManager
      *
      * @param string $name
      * @param string $content
+     * @param User $author
+     * @param Category $category
      * @param string $type
      *
      * @return Conversation
      *
      * @throws \Exception
      */
-    public static function create(string $name, string $content, string $type = 'normal'): Conversation
+    public static function create(string $name, string $content, User $author, Category $category, string $type = 'normal'): Conversation
     {
         $conversation = new Conversation();
         $conversation->setName($name);
         $conversation->setSlug((new Slugify())->slugify($name));
         $conversation->setContent($content);
+        $conversation->setAuthor($author);
+        $conversation->setCategory($category);
         $conversation->setType($type);
 
         return $conversation;
