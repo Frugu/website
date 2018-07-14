b0ee5fb71f40339abb82d2fe4ae3d20c7f368dbe
Baptiste Leduc <baptiste@weglot.com>
2018-07-15 00:17:24 +0200

    making more generic Categories in fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -5,66 +5,51 @@ namespace App\DataFixtures;
 use App\Manager\Forum\CategoryManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class CategoryFixtures extends Fixture
 {
-    public const NON_ROOT_CATEGORIES_COUNT = 11;
+    public const MAX_LEVEL = 3;
+
+    public const ROOT_CATEGORIES_COUNT = 3;
+    public const ROOT_CATEGORIES_PREFIX = 'root-categories-';
+
+    public const NON_ROOT_CATEGORIES_COUNT = 15;
     public const NON_ROOT_CATEGORIES_PREFIX = 'non-root-categories-';
 
+    /**
+     * @param ObjectManager $manager
+     *
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager)
     {
-        $groups = [
-            'EvE Online',
-            'Mac & VIPs',
-        ];
-
-        $forums = [
-            'EvE Online' => [
-                'News & Bullshit' => 'Les meilleures news non vérifiées de tout New Eden...',
-                'Le Bar du Jovien Jovial' => 'Ici on parle de tout mais surtout de rien !',
-                'Corps & Drama' => 'Des corps, des recruteurs mais surtout du drama !',
-                'Wars, Politics & Corpses' => 'Des guerres, des morts et des larmes !',
-                'La hutte de Jabba' => 'Ici, on vends, on achète de tout !',
-            ],
-            'Mac & VIPs' => [
-                'Général' => 'Le forum des vrais, des durs !',
-                'Draft de sujets' => 'Espace de travail pour les futur sujets/annonces/event',
-            ],
-            'Le Bar du Jovien Jovial' => [
-                'Clinic of Battle' => 'Les fits de vos futurs wrecks',
-                'Guides' => '',
-                'Carebear Land' => '',
-            ],
-            'Corps & Drama' => [
-                'Recrutement' => '',
-            ],
-        ];
-
+        $faker = Factory::create('fr_FR');
         $categories = [];
 
-        foreach ($groups as $group) {
-            $category = CategoryManager::create($group, '');
+        for ($i = 0; $i < self::ROOT_CATEGORIES_COUNT; ++$i) {
+            $category = CategoryManager::create($faker->sentence(3), $faker->text(64));
             $category->setType('group');
 
+            $categories[] = $category;
             $manager->persist($category);
-            $categories[$category->getName()] = $category;
+            $this->addReference(self::ROOT_CATEGORIES_PREFIX . $i, $category);
         }
 
+        for ($i = 0; $i < self::NON_ROOT_CATEGORIES_COUNT; ++$i) {
+            $category = CategoryManager::create($faker->sentence(3), $faker->text(64));
 
-        $current = 0;
-        foreach ($forums as $parent => $forum) {
-            $parent = $categories[$parent];
-
-            foreach ($forum as $name => $description) {
-                $category = CategoryManager::create($name, $description);
-                $category->setParent($parent);
-
-                $this->addReference(self::NON_ROOT_CATEGORIES_PREFIX . $current, $category);
-                $manager->persist($category);
-                $categories[$name] = $category;
-
-                ++$current;
+            $random = rand(0, self::ROOT_CATEGORIES_COUNT * 2);
+            if ($random < self::ROOT_CATEGORIES_COUNT) {
+                $parent = $categories[$random];
+            } else {
+                $parent = $categories[rand(self::ROOT_CATEGORIES_COUNT, count($categories) - 1)];
             }
+            $category->setParent($parent);
+
+            $manager->persist($category);
+            $categories[] = $category;
+            $this->addReference(self::NON_ROOT_CATEGORIES_PREFIX . $i, $category);
         }
 
         $manager->flush();
